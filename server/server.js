9301ab95c0cf8e8ef74434ec48e9977340bb62b3
Felipe} <felipebelenoo1101@gmail.com>
2021-09-23 16:36:28 -0500

    changes functionals commit

--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(require('./controllers/index'));
 
